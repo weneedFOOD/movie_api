fb88128d85cb55ff47fe2f4b9e6249c70a92a26b
weneedFOOD <weneedfood.jla@gmail.com>
2023-03-08 22:00:49 -0600

    Update index.js
    feat!: edited GET endpoint to output topMovies list

--- index.js
+++ index.js
@@ -54,21 +54,53 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 
 // setup the logger
 app.use(morgan('combined', {stream: accessLogStream}));
-
-app.use('/documentation.html', express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
-});
-
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
+app.use(express.static('public'));
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+// Welcome Page Text
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+// Get movies
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    movies
+      .find()
+      .then((movie) => res.status(200).json(movie))
+      .catch((err) => {
+        console.err(err);
+        res.status(500).send(`Error: ${err}`);
+      });
+  }
+);
+
+// Get director by name
+app.get(
+  "/movies/director/:name",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { name } = req.params;
+    movies
+      .findOne({ "director.name": name })
+      .then((movie) => {
+        if (movie) {
+          res.status(201).json(movie.director);
+        } else {
+          res.status(400).send(`Director ${name} not found`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send(`Err: ${err}`);
+      });
+  }
+);
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
