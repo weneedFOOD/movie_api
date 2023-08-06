4bacd9c775d04763902fd2030522a4c2c05b3285
weneedFOOD <weneedfood.jla@gmail.com>
2023-08-05 21:23:55 -0500

    Update index.js
    feat!: added token-based authentication on /movies endpoint

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), (
 });
 
 // Get all movies
-app.get('/movies', (req, res) => {
+app.get('/movies',passport.authenticate('jwt', {session: false}), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
